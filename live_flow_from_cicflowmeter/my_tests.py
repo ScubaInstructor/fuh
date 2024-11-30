347e05f7738f96b0a86629a5a19567c4a43260ce
Georg on Nixos <nixos@georgelsas.de>
2024-11-30 12:20:18 +0100

    utilities fertig

--- live_flow_from_cicflowmeter/my_tests.py
+++ live_flow_from_cicflowmeter/my_tests.py
@@ -3,9 +3,10 @@ import joblib
 from my_sniffer import My_Sniffer
 from scapy.sendrecv import AsyncSniffer as ASS
 from cicflowmeter.writer import InternWriter, OutputWriter, output_writer_factory
-from utilities import erstelle_datei
+from utilities import erstelle_datei, sende_BytesIO_datei_per_scp
 from scapy.utils import PcapReader
 from cicflowmeter.flow import Flow
+import os
 
 
 class Tests():    
@@ -28,4 +29,24 @@ class Tests():
         pcap = erstelle_datei(flow)
         assert isinstance(pcap, BytesIO)
         reader = PcapReader(pcap)
-        assert reader.read_all()[0] == flow.packets[0][0] 
+        returning_flows = reader.read_all()
+        for i in range(len(returning_flows)):
+            assert returning_flows[i] == flow.packets[i][0] 
+    
+    def test_sende_per_scp(self):
+        host = "localhost"
+        path = os.getcwd()
+        filename = path+"/"+"test.pcap" # Nur Linux wg '/'
+        sshfile = os.path.expanduser('~')+"/.ssh/id_ed25519.pub"
+        username = "georg"
+        print(path)
+        pcap_BytesIO = erstelle_datei(joblib.load("flow2.pkl"))
+        sende_BytesIO_datei_per_scp(pcap_BytesIO, host, filename, username, sshfile)
+        assert os.path.exists(filename) == True
+        sent_reader = PcapReader(pcap_BytesIO)
+        received_reader = PcapReader(filename)
+        sent_flows = sent_reader.read_all()
+        received_flows = received_reader.read_all()
+        for i in range(len(sent_flows)):
+            assert sent_flows[i] == received_flows[i]
+        os.remove(filename)
--- live_flow_from_cicflowmeter/utilities.py
+++ live_flow_from_cicflowmeter/utilities.py
@@ -15,39 +15,38 @@ def erstelle_datei(flow: flow.Flow) -> BytesIO:
     packete = [p[0] for p in flow.packets]
     
     pcap_buffer = BytesIO()
-    #pcap_buffer = StringIO()
+
     # Schreiben der Pakete in das BytesIO-Objekt
     pktdump = PcapWriter(pcap_buffer)
     for pkt in packete:
         pktdump.write(pkt)
-    
+
     # Zurücksetzen des Zeigers im BytesIO-Objekt
     pcap_buffer.seek(0)
-    #pcap_buffer.close()
 
     return pcap_buffer
 
-# def sende_datei_per_scp(pcap_buffer: BytesIO, ziel_host: str, ziel_pfad: str):
-#     """
-#     Sendet eine pcap-Datei per SCP an einen Zielhost.
+def sende_BytesIO_datei_per_scp(pcap_buffer: BytesIO, ziel_host: str, ziel_pfad: str, username: str, mySSHK: str = '~/.ssh/id_rsa.pub'):
+    """
+    Sendet eine pcap-Datei per SCP an einen Zielhost.
     
-#     Args:
-#         pcap_buffer (io.BytesIO): Das BytesIO-Objekt mit den pcap-Daten.
-#         ziel_host (str): Der Hostname oder die IP-Adresse des Zielhosts.
-#         ziel_pfad (str): Der Pfad auf dem Zielhost, wohin die Datei gesendet werden soll.
-#     """
-#     import paramiko
+    Args:
+        pcap_buffer (io.BytesIO): Das BytesIO-Objekt mit den pcap-Daten.
+        ziel_host (str): Der Hostname oder die IP-Adresse des Zielhosts.
+        ziel_pfad (str): Der Pfad auf dem Zielhost, wohin die Datei gesendet werden soll.
+    """
+    import paramiko
 
-#     # Beispiel für die Verwendung von Paramiko für SCP
-#     ssh = paramiko.SSHClient()
-#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # Beispiel für die Verwendung von Paramiko für SCP
+    ssh = paramiko.SSHClient()
+    # Automatisch hostkey akzeptieren evtl zu unsicher?
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+    # Verbinden mit dem Zielhost
+    ssh.connect(ziel_host, username=username, key_filename=mySSHK)
     
-#     # Verbinden mit dem Zielhost
-#     ssh.connect(ziel_host, username='your_username', password='your_password')
+    # SCP verwenden, um die Datei zu übertragen
+    with ssh.open_sftp() as sftp:
+        with sftp.file(ziel_pfad, 'wb') as remote_file:
+            remote_file.write(pcap_buffer.getvalue())
     
-#     # SCP verwenden, um die Datei zu übertragen
-#     with ssh.open_sftp() as sftp:
-#         with sftp.file(ziel_pfad, 'wb') as remote_file:
-#             remote_file.write(pcap_buffer.getvalue())
-    
-#     ssh.close()
+    ssh.close()
