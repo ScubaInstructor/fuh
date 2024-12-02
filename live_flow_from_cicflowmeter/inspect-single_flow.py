d8ef87c58d38eac42746d11110a9758602885067
Georg on Nixos <nixos@georgelsas.de>
2024-12-02 10:53:08 +0100

    Docker-compose Projekt mit Arkime läuft

--- live_flow_from_cicflowmeter/inspect-single_flow.py
+++ live_flow_from_cicflowmeter/inspect-single_flow.py
@@ -3,7 +3,7 @@ import joblib
 import os
 from cicflowmeter import flow
 from utilities import erstelle_datei, sende_BytesIO_datei_per_scp
-f: flow.Flow = joblib.load('flow1.pkl')
+f: flow.Flow = joblib.load('flow2.pkl')
 
 #%%
 
@@ -11,7 +11,7 @@ flow_bytesio = erstelle_datei(flow=f)
 #%%
 HOST="localhost"
 USER="georg"
-PATH="/tmp/arkime/pcap/test2.pcap"
+PATH="/tmp/arkime/pcap/test1.pcap"
 SSHFILE=os.path.expanduser('~')+"/.ssh/id_ed25519.pub"
 sende_BytesIO_datei_per_scp(pcap_buffer=flow_bytesio, ziel_host=HOST,
                             ziel_pfad=PATH,username=USER, mySSHK=SSHFILE)
