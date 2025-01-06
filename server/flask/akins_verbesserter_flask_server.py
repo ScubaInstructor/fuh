63ccdda011213c033562ef5006110ef8fb8c48e2
Georg on Nixos <nixos@georgelsas.de>
2025-01-06 10:43:31 +0100

    first logout button and refactoring

--- server/flask/akins_verbesserter_flask_server.py
+++ server/flask/akins_verbesserter_flask_server.py
@@ -58,7 +58,7 @@ static_path = abspath(join(getcwd(), 'static/'))
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()  # Create an instance of the form
-    if form.validate_on_submit():  # Check if the form is submitted and valid
+    if form.validate_on_submit():  # Check if the form is submitted and valid, including CSRF
         username = form.username.data
         password = form.password.data
         user = User.query.filter_by(username=username).first()
@@ -173,46 +173,7 @@ def index():
     plt.close()
 
     # Generate HTML content for all available timestamps with sensor names and predictions in a table format
-    return render_template_string("""
-        <html>
-        <head>
-            <title>Abnormal Flows</title>
-            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-        </head>
-        <body class="bg-gray-100">
-            <div class="container mx-auto p-4">
-                <h1 class="text-3xl font-bold mb-4">Abnormal Flows <img src="/static/group2_icon.png" alt="Icon of Group 2" class="w-36 h-36 inline-block mr-2"/></h1>
-                <h2 class="text-xl mb-4">Timeline of the last 60 minutes</h2>
-                <div class="overflow-x-auto">   
-                    <img src="/static/timeline_chart.png" alt="Timeline" class="min-w-full mb-4 rounded shadow-lg" />            
-                    <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
-                        <thead>
-                            <tr class="bg-gray-200 text-gray-600">
-                                <th class="py-2 px-4 border-b">Date</th>
-                                <th class="py-2 px-4 border-b">Sensor</th>
-                                <th class="py-2 px-4 border-b">Predicted class</th>
-                                <th class="py-2 px-4 border-b">Class</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {% for entry in requests %}
-                                {% if not has_been_seen[entry.dataframe_id] %}
-                                    <tr class="hover:bg-gray-100">
-                                        <td class="py-2 px-4 border-b"><a href="/details/{{ entry.dataframe_id }}" class="text-blue-500 hover:underline">{{ entry.timestamp }}</a></td>
-                                        <td class="py-2 px-4 border-b">{{ entry.sensor_name }}</td>
-                                        <td class="py-2 px-4 border-b">{{ entry.prediction }}</td>
-                                        <td class="py-2 px-4 border-b">{{ entry.attack_class | e if entry.attack_class else "unclassified" }}</td>
-                                    </tr>
-                                {% endif %}
-                            {% endfor %}
-                        </tbody>
-                    </table>
-                </div>
-                <button onclick="location.href='/classified_requests'" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Show classified Flows</button>
-            </div>
-        </body>
-        </html>
-    """, requests=[{
+    return render_template('index.html', requests=[{
         'timestamp': timestamps[entry['dataframe_id']],
         'sensor_name': sensor_names[entry['dataframe_id']],
         'prediction': predictions_store[entry['dataframe_id']],
@@ -274,187 +235,18 @@ def details(df_id):
             flag = get_flag_img(ip_lookup.country_name)
             flag_path = f'{static_path}/{ip_lookup.country_code}.png'
             flag.save(flag_path)
-    return render_template_string("""
-        <html>
-        <head>
-            <title>Details for {{ timestamp }}</title>
-            <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-        </head>
-        <body class="bg-gray-100">
-            <div class="container mx-auto p-4">
-                <h1 class="text-xl font-bold mb-4">Details for {{ timestamp }}</h1>
-
-                <div class="flex">
-                    <div class="left-column flex-1 pr-4">
-
-                        <h3 class="text-lg font-semibold mt-4">Prediction</h3>
-                        <p>{{ prediction }}</p>
-
-                        <h3 class="text-lg font-semibold mt-4">Sensor Name</h3>
-                        <p>{{ sensor_name }}</p>
-                        <!-- Dynamische Probabilities-Tabelle -->
-                        <h3 class="text-lg font-semibold mt-4">Probabilities</h3>
-                        {% if probabilities %}
-                            <table class="min-w-full max-w-4xl mx-auto bg-white border border-gray-300 rounded-lg shadow-md mt-4">
-                                <thead>
-                                    <tr class="bg-gray-200 text-gray-600">
-                                        <th class="py-2 px-4 border-b">Class</th>
-                                        <th class="py-2 px-4 border-b">Probability</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {% for key, value in probabilities.items() %}
-                                        {% if value >= 0 %}
-                                            <tr class="hover:bg-gray-100">
-                                                <td class="py-2 px-4 border-b">{{ key | e}}</td>
-                                                <td class="py-2 px-4 border-b">{{ value }}</td>
-                                            </tr>
-                                        {% endif %}
-                                    {% endfor %}
-                                </tbody>
-                            </table>
-                        {% else %}
-                            <p>No probabilities available.</p>
-                        {% endif %}
-
-                        <h3 class="mt-6">Download PCAP File</h3>
-                        <a href="{{ file_download_link }}" class="inline-block bg-blue-500 text-white 
-                                font-bold py-2 px-4 rounded hover:bg-blue-600">Download flow.pcap</a>
-                        
-                        <!-- Dropdown menu for selecting attack class -->
-                        <form method="POST" class="mt-4">
-                                <label for="selected_attack_class" class="block text-sm font-medium text-gray-700">Select Attack Class:</label>
-                            <select name="selected_attack_class" id="selected_attack_class" class="mt-1 block w-full p-2 border border-gray-300 rounded-md">
-                                <option value="" disabled selected>Select...</option>
-                                {% for key, value in probabilities.items() %}
-                                    <option value="{{ key }}">{{ key }}</option>
-                                {% endfor %}
-                                <option value="NEW_CLASS">Create New Class</option>
-                            </select><br />
-
-                            <!-- Input field for new class if selected -->
-                            <div id="new-class-input" style="display:none;">
-                                <label for="new_class_name" class="block text-sm font-medium text-gray-700 mt-2">New Class Name:</label><br />
-                                <input type="text" name="new_class_name" id="new_class_name" placeholder="Enter new class name" 
-                                    class="mt-1 block w-full p-2 border border-gray-300 rounded-md"
-                                    pattern="^[A-Za-z0-9_]+$" required title="Only letters, numbers and underscore are allowed"/>
-                            </div>
-
-                            <!-- Submit button -->
-                            <button type="submit" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Submit</button>
-                        </form>
-                        <!-- JavaScript to show/hide new class input based on dropdown selection -->
-                        <script type="text/javascript">
-                            const selectElement = document.getElementById('selected_attack_class');
-                            const newClassInputDiv = document.getElementById('new-class-input');
-                            
-                            selectElement.addEventListener('change', function() {
-                                if (this.value === 'NEW_CLASS') {
-                                    newClassInputDiv.style.display = 'block';
-                                } else {
-                                    newClassInputDiv.style.display = 'none';
-                                }
-                            });
-                        </script>
-
-                    </div>
-                                  
-
-                    <!-- Right Column -->
-                    <div class="right-column w-1/3">
-                        <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md mt-4"> 
-                            <thead> 
-                                <tr class="bg-gray-200 text-gray-600"> 
-                                    <th class="py-2 px-4 border-b items-center">
-                                        <img src="/static/group2_icon.png" alt=""Icon of this project"  class="w-12 h-12 inline-block mr-2"/> {{sensor_name}}
-                                    </th>
-                                    {% if private_ip %}
-                                        <th class="py-2 px-4 border-b items-center">
-                                            {{partner_ip}} <img src="/static/private.png" alt=""Flag for {{partner_ip}}" class="w-16 h-9 inline-block ml-2"/>
-                                        </th>
-                                    {% else %}
-                                        <th class="py-2 px-4 border-b items-center">
-                                            {{partner_ip}} <img src="/static/{{ flag_country_code }}.png" alt="Flag for {{partner_ip}}" class="w-16 h-9 inline-block ml-2"/>
-                                        </th>
-                                    {% endif %} 
-                                </tr>
-                            </thead> 
-                            <tbody> 
-                                <tr class="hover:bg-gray-100"> 
-                                    <td class="py-2 px-4 border-b">{{sensor_port}}</td>  
-                                    <td class="py-2 px-4 border-b">{{partner_port}}</td> 
-                                </tr>
-                            </tbody> 
-                        </table>              
-                    
-                    <!-- Canvas for Pie Chart -->
-                    <div class="mt-6">
-                        <canvas id="probabilities-chart" width="300" height="300"></canvas>
-                    </div>
-                    <!-- JavaScript to create the pie chart -->
-                    <script>
-                        const probabilitiesData = {{ probabilities | tojson }};
-                        const filteredEntries = Object.entries(probabilitiesData).filter(([key, value]) => value >= 0); //Don't use newly created classes!
-                        const labels = filteredEntries.map(entry => entry[0]); // Labels
-                        const dataValues = filteredEntries.map(entry => entry[1]); // Values
-
-                        const ctx = document.getElementById('probabilities-chart');
-                        const chart = new Chart(ctx, {
-                            type: 'pie',
-                            data: {
-                                labels: labels,
-                                datasets: [{
-                                    label: 'Probability Distribution',
-                                    data: dataValues,
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)',
-                                        'rgba(255, 159, 64, 0.2)'
-                                    ],
-                                    borderColor: [
-                                        'rgba(255, 99, 132, 1)',
-                                        'rgba(54, 162, 235, 1)',
-                                        'rgba(255, 206, 86, 1)',
-                                        'rgba(75, 192, 192, 1)',
-                                        'rgba(153, 102, 255, 1)',
-                                        'rgba(255, 159, 64, 1)'
-                                    ],
-                                    borderWidth: 1
-                                }]
-                            },
-                            options: {
-                                responsive: true,
-                                plugins: {
-                                    legend: {
-                                        position: 'top',
-                                    },
-                                    title: {
-                                        display: true,
-                                        text: 'Probability Distribution'
-                                    }
-                                }
-                            }
-                        });
-                    </script>
-                </div>
-                                  
-        </body>
-        </html>
-    """, probabilities=probabilities,
-       prediction=prediction,
-       sensor_name=sensor_name,
-       timestamp=timestamp,
-       file_download_link=file_download_link,
-       sensor_port=sensor_port,
-       partner_ip=partner_ip,
-       partner_port=partner_port,
-       flag_country_code=flag_country_code,
-       private_ip=private_ip # If Country flag must be used
-       )
+    return render_template('details.html', 
+        probabilities=probabilities,
+        prediction=prediction,
+        sensor_name=sensor_name,
+        timestamp=timestamp,
+        file_download_link=file_download_link,
+        sensor_port=sensor_port,
+        partner_ip=partner_ip,
+        partner_port=partner_port,
+        flag_country_code=flag_country_code,
+        private_ip=private_ip # If Country flag must be used
+        )
 
 
 
@@ -466,15 +258,6 @@ def some_function():
     print(trainingdata)
     print("DEBUG: Button pushed")
     return redirect(url_for('index'))  # Back to index
-
-# @app.route('/get_dataframe/<df_id>', methods=['GET'])
-# def get_dataframe(df_id):
-#     if df_id in dataframes:
-#         df = dataframes[df_id]
-#         df_json = df.to_dict(orient='records')
-#         return jsonify({"data": df_json})
-#     return jsonify({"error": "DataFrame not found"}), 404
-
 
 
 @app.route('/download/<file_id>')
@@ -493,43 +276,7 @@ def download_file(file_id):
 @login_required
 def classified_requests():
     # HTML for classified Flows
-    return render_template_string("""
-        <html>
-            <head>
-                <title>Already classified anomalies</title>
-                <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-            </head>
-            <body class="bg-gray-100">
-                <div class="container mx-auto p-4">
-                    <h1 class="text-3xl font-bold mb-4">Already classified anomalies</h1>
-                    <table class="min-w-full bg-white border border-gray-300 rounded-lg shadow-md">
-                        <thead>
-                            <tr class="bg-gray-200 text-gray-600">
-                                <th class="py-2 px-4 border-b">Date</th>
-                                <th class="py-2 px-4 border-b">Sensor</th>
-                                <th class="py-2 px-4 border-b">Predicted class</th>
-                                <th class="py-2 px-4 border-b">Class</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {% for entry in requests %}
-                                {% if has_been_seen[entry.dataframe_id] %}
-                                    <tr class="hover:bg-gray-100">
-                                        <td class="py-2 px-4 border-b"><a href="/details/{{ entry.dataframe_id }}" class="text-blue-500 hover:underline">{{ entry.timestamp }}</a></td>
-                                        <td class="py-2 px-4 border-b">{{ entry.sensor_name }}</td>
-                                        <td class="py-2 px-4 border-b">{{ entry.prediction }}</td>
-                                        <td class="py-2 px-4 border-b">{{ entry.attack_class | e if entry.attack_class else "unklassifiziert" }}</td>
-                                    </tr>
-                                {% endif %}
-                            {% endfor %}
-                        </tbody>
-                    </table>
-                    <button onclick="location.href='/'" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Start Page</button>
-                    <button onclick="location.href='/retrain'" class="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Retrain</button>
-                </div>
-            </body>
-        </html>
-    """, requests=[{
+    return render_template('classified_requests.html', requests=[{
             'timestamp': timestamps[entry['dataframe_id']],
             'sensor_name': sensor_names[entry['dataframe_id']],
             'prediction': predictions_store[entry['dataframe_id']],
