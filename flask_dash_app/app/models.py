3ba3b4d580efc4ed4ee319754c74dd8dab8466b7
Georg on Nixos <nixos@georgelsas.de>
2025-02-03 11:06:33 +0100

    salt for passwords has been added. users.db needs to be recreated!

--- flask_dash_app/app/models.py
+++ flask_dash_app/app/models.py
@@ -1,3 +1,5 @@
+import base64
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import UserMixin
@@ -6,13 +8,11 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), default='user', nullable=False)  # ← New column
+    salt = db.Column(db.String(150), nullable=False)  # Salt for the password
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.salt =  base64.b64encode(os.urandom(150)).decode('utf-8')
+        self.password_hash = generate_password_hash(password + self.salt)
 
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
-    def is_admin(self):
-        return self.role == 'admin'
+        return check_password_hash(self.password_hash, password + self.salt)
